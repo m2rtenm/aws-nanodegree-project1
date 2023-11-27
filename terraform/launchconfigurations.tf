ac509019294a8418591484afe66b4f4925a15ecd
Märten Mändla <marten.mandla@gmail.com>
2023-11-28 00:13:31 +0200

    Readme done

--- terraform/launchconfigurations.tf
+++ terraform/launchconfigurations.tf
@@ -1,5 +1,13 @@
+data "aws_ami" "packer_ami" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = [ "packer*" ]
+  }
+}
+
 resource "aws_instance" "bastion" {
-  ami = var.packer_ami
+  ami = data.aws_ami.packer_ami.image_id
   instance_type = var.instance_type
   key_name = aws_key_pair.keypair.key_name
   associate_public_ip_address = false
@@ -12,7 +20,7 @@ resource "aws_instance" "bastion" {
 
 resource "aws_launch_configuration" "ec2" {
   name = "${var.prefix}-instances-lc"
-  image_id = var.packer_ami
+  image_id = data.aws_ami.packer_ami.image_id
   instance_type = var.instance_type
   security_groups = [ aws_security_group.ec2.id ]
   key_name = aws_key_pair.keypair.key_name
